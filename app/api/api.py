a11a6c11614c6eeaf662d42c2c61b9910af5ecfe
hoangtrung225 <hoangtrung225@gmail.com>
2019-05-26 18:36:43 +0100

    add select number data sample

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,7 @@
 from app import app
 from app.models import Sensor 
 from flask import jsonify
+from flask import request
 
 @app.route('/api/sensor/update', methods=['GET'])
 def sensor_update():
@@ -38,8 +39,14 @@ def get_sensor_timeline(node_id, view):
 
     if not view in ['light1', 'light2', 'temperature', 'humidity']:
         return None
-    #query database get all record about sensor node
-    records = Sensor.query.filter(Sensor.node_id == node_id)
+    query = request.args.get("query")
+    reverse = False
+    if query != None and query.isdigit():
+        records = Sensor.query.filter(Sensor.node_id == node_id).order_by(Sensor.created_date.desc()).limit(query)
+        reverse = True
+    else:
+        #query database get all record about sensor node
+        records = Sensor.query.filter(Sensor.node_id == node_id)
     sensor_data = {"view": view, "sensor": node_id}
     timeline_data = []
     for record in records:
@@ -48,6 +55,8 @@ def get_sensor_timeline(node_id, view):
                 "time": data["sys_time"],
                 "data": data.get(view)
             })
+    if reverse == True:
+        timeline_data.reverse()
     sensor_data.update({"data": timeline_data})
 
     return jsonify(sensor_data)
